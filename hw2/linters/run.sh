1737def7b0601344e731664e8a253d956f899b46
tupiznak <akej-vonavi@mail.ru>
2022-10-22 05:55:38 +0000

    prepare folders

---
+++ hw2/linters/run.sh
@@ -0,0 +1,33 @@
+#!/usr/bin/env bash
+
+set -o pipefail
+
+function print_header() {
+    echo -e "\n***** ${1} *****"
+}
+
+function check_log() {
+    LOG=$( { ${1}; } 2>&1 )
+    STATUS=$?
+    echo "$LOG"
+    if echo "$LOG" | grep -q -E "${2}"
+    then
+        exit 1
+    fi
+
+    if [ $STATUS -ne 0 ]
+    then
+        exit $STATUS
+    fi
+}
+
+print_header "RUN cppcheck"
+check_log "cppcheck matrix_operations_lib --enable=all --inconclusive --error-exitcode=1 -I matrix_operations_lib/ --suppress=missingIncludeSystem --suppress=unreadVariable --suppress=unusedFunction" "\(information\)"
+
+print_header "RUN clang-tidy"
+check_log "clang-tidy matrix_operations_lib/matrix_operations.c -extra-arg=-std=c99 -checks='-*,bugprone-*,cert-dcl21-cpp,cert-dcl50-cpp,cert-env33-c,cert-err34-c,cert-err52-cpp,cert-err60-cpp,cert-flp30-c,cert-msc50-cpp,cert-msc51-cpp,cppcoreguidelines-*,-cppcoreguidelines-macro-usage,-cppcoreguidelines-pro-type-reinterpret-cast,-cppcoreguidelines-pro-type-union-access,-cppcoreguidelines-pro-bounds-pointer-arithmetic,-cppcoreguidelines-pro-type-vararg,google-build-using-namespace,google-explicit-constructor,google-global-names-in-headers,google-readability-casting,google-runtime-int,google-runtime-operator,hicpp-*,-hicpp-vararg,misc-*,modernize-*,performance-*,readability-*,-readability-named-parameter' -- -Imatrix_operations_lib/" "Error (?:reading|while processing)"
+
+print_header "RUN cpplint"
+check_log "cpplint matrix_operations_lib/*" "Can't open for reading"
+
+print_header "SUCCESS"

41cce2ff417ca396f549343c84f45bbd9b9c0b4d
tupiznak <akej-vonavi@mail.ru>
2022-10-26 13:32:37 +0000

    remove calc

--- hw2/matrix_calc_lib/matrix_calc+private.cpp
+++
@@ -1,9 +0,0 @@
-#include "matrix_calc+private.h"
-
-void Matrix::MatrixImpl::SetItems(std::vector<double> const &data) {
-    items = data;
-}
-
-std::vector<double> &Matrix::MatrixImpl::GetItems() {
-    return items;
-};

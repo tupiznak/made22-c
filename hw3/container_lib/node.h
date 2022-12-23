0d6eb5ec8cec5074d8c5be8a2f921e363cef3a6b
tupiznak <akej-vonavi@mail.ru>
2022-12-23 06:33:25 +0000

    move node methods to set

--- hw3/container_lib/node.h
+++ hw3/container_lib/node.h
@@ -21,63 +21,6 @@ namespace hw3 {
             if (left != nullptr) { delete left; }
             if (right != nullptr) { delete right; }
         }
-        inline Node *smallRotateRight(Node *joint_vertex) noexcept {
-            auto *const p = joint_vertex;
-            auto *const q = joint_vertex->left;
-            p->left = q->right;
-            q->right = p;
-
-            if (p->left != nullptr) { p->left->parent = p; }
-            q->parent = p->parent;
-            p->parent = q;
-
-            p->updateHeight();
-            q->updateHeight();
-            return q;
-        }
-        inline Node *smallRotateLeft(Node *joint_vertex) noexcept {
-            auto *const p = joint_vertex;
-            auto *const q = joint_vertex->right;
-            p->right = q->left;
-            q->left = p;
-
-            if (p->right != nullptr) { p->right->parent = p; }
-            q->parent = p->parent;
-            p->parent = q;
-
-            p->updateHeight();
-            q->updateHeight();
-            return q;
-        }
-        inline Node *bigRotateRight(Node *joint_vertex) noexcept {
-            auto *const p = joint_vertex;
-            auto *const q = joint_vertex->left;
-            p->left = q->smallRotateLeft(q);
-            return p->smallRotateRight(p);
-        }
-        inline Node *bigRotateLeft(Node *joint_vertex) noexcept {
-            auto *const p = joint_vertex;
-            auto *const q = joint_vertex->right;
-            p->right = q->smallRotateRight(q);
-            return p->smallRotateLeft(p);
-        }
-
-        Node *balance(Node *joint_vertex) {
-            const auto curr_balance = joint_vertex->getBalance();
-            joint_vertex->updateHeight();
-            if (curr_balance > 1) {
-                if (joint_vertex->right != nullptr && joint_vertex->right->getBalance() < 0) {
-                    return joint_vertex->bigRotateLeft(joint_vertex);
-                }
-                return joint_vertex->smallRotateLeft(this);
-            } else if (curr_balance < -1) {
-                if (joint_vertex->left != nullptr && joint_vertex->left->getBalance() > 0) {
-                    return joint_vertex->bigRotateRight(joint_vertex);
-                }
-                return joint_vertex->smallRotateRight(this);
-            }
-            return this;
-        }
 
         inline void updateHeight() noexcept {
             const auto height_left = getHeight(left);
@@ -93,68 +36,7 @@ namespace hw3 {
         }
         inline int getBalance() const noexcept { return getHeight(right) - getHeight(left); }
 
-        static Node *insert(Node *curr_vertex, Node *parent_vertex, const_reference key) {
-            if (curr_vertex == nullptr) {
-                curr_vertex = new Node<T>(key, parent_vertex);
-                return curr_vertex;
-            }
-            if (key < curr_vertex->_key) {
-                auto *const sub_tree = insert(curr_vertex->left, curr_vertex, key);
-                if (sub_tree == nullptr) { return nullptr; }
-                curr_vertex->left = sub_tree;
-            } else if (curr_vertex->_key < key) {
-                auto *const sub_tree = insert(curr_vertex->right, curr_vertex, key);
-                if (sub_tree == nullptr) { return nullptr; }
-                curr_vertex->right = sub_tree;
-            } else { return nullptr; }
-            return curr_vertex->balance(curr_vertex);
-        };
-        static Node *erase(Node *curr_vertex, Node *parent_vertex, const_reference key) {
-            if (curr_vertex == nullptr) { return nullptr; }
-            if (key < curr_vertex->_key) {
-                curr_vertex->left = erase(curr_vertex->left, curr_vertex, key);
-            } else if (curr_vertex->_key < key) {
-                curr_vertex->right = erase(curr_vertex->right, curr_vertex, key);
-            } else if (curr_vertex->right == nullptr && curr_vertex->left == nullptr) {
-                if (parent_vertex) {
-                    parent_vertex->left == curr_vertex ? parent_vertex->left = nullptr : parent_vertex->right = nullptr;
-                }
-                delete curr_vertex;
-                curr_vertex = nullptr;
-            } else if (curr_vertex->left == nullptr) {
-                auto *right = curr_vertex->right;
-                if (parent_vertex) {
-                    parent_vertex->left == curr_vertex ? parent_vertex->left = right : parent_vertex->right = right;
-                }
-                right->parent = parent_vertex;
-                curr_vertex->right = nullptr;
-                delete curr_vertex;
-                curr_vertex = right;
-            } else if (curr_vertex->right == nullptr) {
-                auto *left = curr_vertex->left;
-                if (parent_vertex) {
-                    parent_vertex->left == curr_vertex ? parent_vertex->left = left : parent_vertex->right = left;
-                }
-                left->parent = parent_vertex;
-                curr_vertex->left = nullptr;
-                delete curr_vertex;
-                curr_vertex = left;
-            } else {
-                curr_vertex->_key = findMax(curr_vertex->left)->_key;
-                curr_vertex->left = erase(curr_vertex->left, curr_vertex, curr_vertex->_key);
-            }
-            if (curr_vertex == nullptr) { return nullptr; }
-            return curr_vertex->balance(curr_vertex);
-        };
-
-        static Node *contains(Node *curr_vertex, const_reference key) {
-            if (curr_vertex == nullptr) { return curr_vertex; }
-            if (key < curr_vertex->_key) { return contains(curr_vertex->left, key); }
-            else if (curr_vertex->_key < key) { return contains(curr_vertex->right, key); }
-            return curr_vertex;
-        }
-
-        inline static Node *findMax(Node *curr_vertex) noexcept {
+        inline static Node *findMax(Node<value_type> *curr_vertex) noexcept {
             if (curr_vertex == nullptr) { return nullptr; }
             while (curr_vertex->right != nullptr) { curr_vertex = curr_vertex->right; }
             return curr_vertex;
@@ -165,14 +47,6 @@ namespace hw3 {
             while (curr_vertex->left != nullptr) { curr_vertex = curr_vertex->left; }
             return curr_vertex;
         }
-
-        static void printTree(Node *curr_vertex) noexcept {
-            if (curr_vertex == nullptr) { return; }
-            printTree(curr_vertex->left);
-            std::cout << curr_vertex->_key << " ";
-            printTree(curr_vertex->right);
-        }
-
         Node *prev() noexcept {
             if (left != nullptr) { return findMax(left); }
             if (parent == nullptr || parent->right == this) { return parent; }
@@ -195,6 +69,9 @@ namespace hw3 {
 
 
     private:
+        template<class U>
+        friend class Set;
+
         value_type _key;
         unsigned height{1};
         Node *left{};
--- hw3/container_lib/set.h
+++ hw3/container_lib/set.h
@@ -43,17 +43,17 @@ namespace hw3 {
         ~Set() { delete root; }
 
         void insert(const_reference key) {
-            auto *const tree = Node<value_type>::insert(root, nullptr, key);
+            auto *const tree = _insert(root, nullptr, key);
             if (tree == nullptr) { return; }
             root = tree;
             ++elements_count;
         };
         void erase(const_reference key) {
             if (!contains(key)) { return; }
-            root = Node<value_type>::erase(root, nullptr, key);
+            root = _erase(root, nullptr, key);
             --elements_count;
         };
-        bool contains(const_reference key) { return Node<value_type>::contains(root, key) != nullptr; };
+        bool contains(const_reference key) { return _contains(root, key) != nullptr; };
         void printTree() { Node<value_type>::printTree(root); }
         SetIterator<value_type> lower_bound(const_reference key) const {
             auto *curr_vertex = root;
@@ -92,10 +92,132 @@ namespace hw3 {
             return ++SetIterator<value_type>(Node<value_type>::findMax(root));
         }
         SetIterator<value_type> find(const_reference key) const {
-            const auto *vertex = Node<value_type>::contains(root, key);
+            const auto *vertex = _contains(root, key);
             return vertex != nullptr ? SetIterator<value_type>(vertex) : end();
         }
+
     private:
+        inline Node<value_type> *_smallRotateRight(Node<value_type> *joint_vertex) const noexcept {
+            auto *const p = joint_vertex;
+            auto *const q = joint_vertex->left;
+            p->left = q->right;
+            q->right = p;
+
+            if (p->left != nullptr) { p->left->parent = p; }
+            q->parent = p->parent;
+            p->parent = q;
+
+            p->updateHeight();
+            q->updateHeight();
+            return q;
+        }
+        inline Node<value_type> *_smallRotateLeft(Node<value_type> *joint_vertex) const noexcept {
+            auto *const p = joint_vertex;
+            auto *const q = joint_vertex->right;
+            p->right = q->left;
+            q->left = p;
+
+            if (p->right != nullptr) { p->right->parent = p; }
+            q->parent = p->parent;
+            p->parent = q;
+
+            p->updateHeight();
+            q->updateHeight();
+            return q;
+        }
+        inline Node<value_type> *_bigRotateRight(Node<value_type> *joint_vertex) const noexcept {
+            auto *const p = joint_vertex;
+            auto *const q = joint_vertex->left;
+            p->left = _smallRotateLeft(q);
+            return _smallRotateRight(p);
+        }
+        inline Node<value_type> *_bigRotateLeft(Node<value_type> *joint_vertex) const noexcept {
+            auto *const p = joint_vertex;
+            auto *const q = joint_vertex->right;
+            p->right = _smallRotateRight(q);
+            return _smallRotateLeft(p);
+        }
+
+        Node<value_type> *_balance(Node<value_type> *joint_vertex) const {
+            const auto curr_balance = joint_vertex->getBalance();
+            joint_vertex->updateHeight();
+            if (curr_balance > 1) {
+                if (joint_vertex->right != nullptr && joint_vertex->right->getBalance() < 0) {
+                    return _bigRotateLeft(joint_vertex);
+                }
+                return _smallRotateLeft(joint_vertex);
+            } else if (curr_balance < -1) {
+                if (joint_vertex->left != nullptr && joint_vertex->left->getBalance() > 0) {
+                    return _bigRotateRight(joint_vertex);
+                }
+                return _smallRotateRight(joint_vertex);
+            }
+            return joint_vertex;
+        }
+
+        Node<value_type> *
+        _insert(Node<value_type> *curr_vertex, Node<value_type> *parent_vertex, const_reference key) const {
+            if (curr_vertex == nullptr) {
+                curr_vertex = new Node<value_type>(key, parent_vertex);
+                return curr_vertex;
+            }
+            if (key < curr_vertex->_key) {
+                auto *const sub_tree = _insert(curr_vertex->left, curr_vertex, key);
+                if (sub_tree == nullptr) { return nullptr; }
+                curr_vertex->left = sub_tree;
+            } else if (curr_vertex->_key < key) {
+                auto *const sub_tree = _insert(curr_vertex->right, curr_vertex, key);
+                if (sub_tree == nullptr) { return nullptr; }
+                curr_vertex->right = sub_tree;
+            } else { return nullptr; }
+            return _balance(curr_vertex);
+        };
+        Node<value_type> *
+        _erase(Node<value_type> *curr_vertex, Node<value_type> *parent_vertex, const_reference key) const {
+            if (curr_vertex == nullptr) { return nullptr; }
+            if (key < curr_vertex->_key) {
+                curr_vertex->left = _erase(curr_vertex->left, curr_vertex, key);
+            } else if (curr_vertex->_key < key) {
+                curr_vertex->right = _erase(curr_vertex->right, curr_vertex, key);
+            } else if (curr_vertex->right == nullptr && curr_vertex->left == nullptr) {
+                if (parent_vertex) {
+                    parent_vertex->left == curr_vertex ? parent_vertex->left = nullptr : parent_vertex->right = nullptr;
+                }
+                delete curr_vertex;
+                curr_vertex = nullptr;
+            } else if (curr_vertex->left == nullptr) {
+                auto *right = curr_vertex->right;
+                if (parent_vertex) {
+                    parent_vertex->left == curr_vertex ? parent_vertex->left = right : parent_vertex->right = right;
+                }
+                right->parent = parent_vertex;
+                curr_vertex->right = nullptr;
+                delete curr_vertex;
+                curr_vertex = right;
+            } else if (curr_vertex->right == nullptr) {
+                auto *left = curr_vertex->left;
+                if (parent_vertex) {
+                    parent_vertex->left == curr_vertex ? parent_vertex->left = left : parent_vertex->right = left;
+                }
+                left->parent = parent_vertex;
+                curr_vertex->left = nullptr;
+                delete curr_vertex;
+                curr_vertex = left;
+            } else {
+                curr_vertex->_key = Node<value_type>::findMax(curr_vertex->left)->_key;
+                curr_vertex->left = _erase(curr_vertex->left, curr_vertex, curr_vertex->_key);
+            }
+            if (curr_vertex == nullptr) { return nullptr; }
+            return _balance(curr_vertex);
+        };
+
+        Node<value_type> *_contains(Node<value_type> *curr_vertex, const_reference key) const {
+            if (curr_vertex == nullptr) { return curr_vertex; }
+            if (key < curr_vertex->_key) { return _contains(curr_vertex->left, key); }
+            else if (curr_vertex->_key < key) { return _contains(curr_vertex->right, key); }
+            return curr_vertex;
+        }
+
         Node<value_type> *root{};
         unsigned elements_count{0};
     };
